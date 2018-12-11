7ba09c43036a389f66c09a5b72c9fc85ca2a92c9
admin <alexe-mgn@mail.ru>
2018-12-11 20:16:01 +0500

    initial file created

---
+++ project.py
@@ -0,0 +1,21 @@
+import sys
+import psutil
+import PyQt5.QtWidgets as Qw
+import PyQt5.QtGui as Qg
+import PyQt5.QtCore as Qc
+
+
+class Main:
+
+    def __init__(self):
+        pass
+
+    def show(self):
+        pass
+
+
+if __name__ == '__main__':
+    app = Qw.QApplication(sys.argv)
+    main = Main()
+    main.show()
+    sys.exit(app.exec())
