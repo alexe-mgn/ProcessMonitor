bcf2fa8b5396e10fd3c6ba6f4274ddd3a6b61c07
marienov <senya2104@gmail.com>
2018-12-14 03:06:30 +0500

    started to creat ProcessTab

--- process_tab.py
+++ process_tab.py
@@ -6,6 +6,10 @@ import PyQt5.QtCore as Qc
 import pyqtgraph as Pg
 import time
 
+APP_NAME = 'Process Monitor'
+ORG_NAME = 'Project1'
+TAB_MINIMUM_SIZE = [300, 300]
+
 
 class CustomGraph(Pg.PlotWidget):
 
@@ -213,24 +217,170 @@ class ProcessWidget(Qw.QGroupBox):
     def count_proc(self):
         return len(self.processes)
 
+
+
+class ProcessTab(Qw.QWidget):
+
+    def __init__(self, parent):
+        super().__init__()
+        self.init_ui()
+
+
+    def init_ui(self):
+
+        self.proc_wds=[]
+        self.layout = Qw.QVBoxLayout(self)
+        for e in range(len(self.get_processes())):
+            self.proc_wds.append(ProcessWidget(self,proc_name=self.get_processes()[e]))
+            self.layout.addWidget(self.proc_wds[e])
+
+
+    def update_info(self):
+        for e in self.proc_wds:
+            e.update_info()
+
+
+    def passive_update(self):
+        pass
+
+    def get_processes(self):
+        self.processes = list(set(map(lambda e: e.name(),list( psutil.process_iter()))))
+        return self.processes
+
+    def delete_dead_proc(self):
+        pass
+
+class SettingsTab(Qw.QWidget):
+
+    def __init__(self, parent):
+        super().__init__()
+        self.init_ui()
+
+    def init_ui(self):
+        pass
+
+    def update_info(self):
+        pass
+
+    def passive_update(self):
+        pass
+
+
+class GraphsTab(Qw.QWidget):
+
+    def __init__(self, parent):
+        super().__init__()
+        self.init_ui()
+
+    def init_ui(self):
+        pass
+
+    def update_info(self):
+        pass
+
+    def passive_update(self):
+        pass
+
+
 class Main:
+
     def __init__(self):
-        self.window = Qw.QWidget()
-        self.window.setGeometry(0, 0, 300, 300)
-        layout = Qw.QVBoxLayout(self.window)
-        self.wd = ProcessWidget(proc_name='chrome.exe')
-        layout.addWidget(self.wd)
-        self.timer = Qc.QTimer(self.window)
-        self.timer.start(100)
-        self.timer.timeout.connect(self.wd.update_info)
+        self.timing = False
+        self.read_settings()
+        self.init_ui()
+        self.init_timers()
+        self.start_timers()
+
+    def init_timers(self):
+        self.timer = Qc.QTimer(self.main_window)
+        self.timer.setInterval(1000 / self.update_frequency)
+        self.timer.timeout.connect(self.update_info)
+        self.passive_timer = Qc.QTimer(self.main_window)
+        self.passive_timer.setInterval(self.passive_period * 1000)
+        self.passive_timer.timeout.connect(self.passive_update)
+
+    def start_timers(self):
+        self.timing = True
+        self.timer.start()
+        self.passive_timer.start()
+
+    def stop_timers(self):
+        self.timing = False
+        self.timer.stop()
+        self.passive_timer.stop()
+
+    def kill_timers(self):
+        self.timer.killTimer(self.timer.timerId())
+        self.passive_timer.killTimer(self.passive_timer.timerId())
+
+    def init_ui(self):
+        # Main window
+        self.main_window = Qw.QWidget()
+        self.main_window.setWindowTitle(APP_NAME)
+        self.main_window.setGeometry(300, 300, 300, 300)
+        self.main_window.setMinimumSize(300, 300)
+        self.layout = Qw.QVBoxLayout(self.main_window)
+        self.layout.setContentsMargins(0, 0, 0, 0)
+        self.layout.setSpacing(0)
+
+        # Tabs bar
+        self.tabs = Qw.QTabBar()
+        self.tabs.setFixedHeight(20)
+        self.tabs.addTab('Processes')
+        self.tabs.addTab('Graphs')
+        self.tabs.addTab('Settings')
+        self.tabs.currentChanged.connect(self.change_tab)
+        self.layout.addWidget(self.tabs)
+
+        # Tab scroll area
+        self.scroll = Qw.QScrollArea(self.main_window)
+        self.scroll.layout = Qw.QVBoxLayout(self.scroll)
+        self.scroll.layout.setContentsMargins(0, 0, 0, 0)
+        self.layout.addWidget(self.scroll)
+
+        # Tabs init
+        self.tab_ind = 0
+        self.tab_widgets = [ProcessTab(self), GraphsTab(self), SettingsTab(self)]
+        self.scroll.layout.addWidget(self.tab_widgets[0])
+
+    def change_tab(self, ind):
+        self.current_tab().setParent(None)
+        self.tab_ind = ind
+        self.tab_widgets[ind].update_info()
+        self.scroll.layout.addWidget(self.tab_widgets[ind])
+
+    def update_info(self):
+        self.current_tab().update_info()
+
+    def passive_update(self):
+        for i in self.tab_widgets:
+            i.passive_update()
+
+    def read_settings(self):
+        self.settings = Qc.QSettings()
+        self.update_frequency = float(self.settings.value('update frequency', 10))
+        self.passive_period = float(self.settings.value('passive period', 5))
+        if self.timing:
+            self.kill_timers()
+            self.init_timers()
+            self.start_timers()
+
+    def current_tab(self):
+        return self.tab_widgets[self.tab_ind]
 
     def show(self):
-        self.window.show()
+        self.main_window.show()
 
+
+def except_hook(cls, exception, traceback):
+    sys.__excepthook__(cls, exception, traceback)
 
 
 if __name__ == '__main__':
+    sys.excepthook = except_hook
     app = Qw.QApplication(sys.argv)
+    app.setApplicationName(APP_NAME)
+    app.setOrganizationName(ORG_NAME)
     main = Main()
     main.show()
     sys.exit(app.exec())
