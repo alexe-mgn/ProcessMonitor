b45fa4fa6f5520744c05435f4b33e12e32e34fa6
alexe-mgn <alexe-mgn@mail.ru>
2018-12-13 16:43:30 +0500

    Added timer support

--- settings_tab.py
+++ settings_tab.py
@@ -5,6 +5,10 @@ import PyQt5.QtGui as Qg
 import PyQt5.QtCore as Qc
 
 
+APP_NAME = 'Process Monitor'
+ORG_NAME = 'Project1'
+
+
 class ProcessTab(Qw.QWidget):
 
     def __init__(self, parent=None):
@@ -69,9 +73,11 @@ class SettingsTab(Qw.QWidget):
         self.button.clicked.connect(self.apply())
 
     def apply(self):
+        self.settings = main.settings
         self.pass_freq = self.spin_pas.value()
         self.freq = self.freq.value()
         self.check = self.check_b.isChecked()
+        main.read_settings()
 
     def update_info(self):
         pass
@@ -80,10 +86,37 @@ class SettingsTab(Qw.QWidget):
 class Main:
 
     def __init__(self):
+        self.timing = False
+        self.read_settings()
         self.init_ui()
+        self.init_timers()
+        self.start_timers()
+
+    def init_timers(self):
+        self.timer = Qc.QTimer(self.main_window)
+        self.timer.setInterval(1000 / self.update_frequency)
+        self.timer.timeout.connect(self.update_info)
+        self.passive_timer = Qc.QTimer(self.main_window)
+        self.passive_timer.setInterval(self.passive_period * 1000)
+        self.passive_timer.timeout.connect(self.passive_update)
+
+    def start_timers(self):
+        self.timing = True
+        self.timer.start()
+        self.passive_timer.start()
+
+    def stop_timers(self):
+        self.timing = False
+        self.timer.stop()
+        self.passive_timer.stop()
+
+    def kill_timers(self):
+        self.timer.killTimer(self.timer.timerId())
+        self.passive_timer.killTimer(self.passive_timer.timerId())
 
     def init_ui(self):
         self.main_window = Qw.QWidget()
+        self.main_window.setWindowTitle(APP_NAME)
         self.main_window.setGeometry(300, 300, 300, 300)
         self.layout = Qw.QVBoxLayout(self.main_window)
         self.layout.setContentsMargins(0, 0, 0, 0)
@@ -98,17 +131,36 @@ class Main:
         self.layout.addWidget(self.tabs)
 
         self.scroll = Qw.QScrollArea(self.main_window)
+        self.scroll.setWidgetResizable(True)
         self.layout.addWidget(self.scroll)
 
+        self.tab_ind = 0
         self.tab_widgets = [ProcessTab(), GraphsTab(), SettingsTab()]
         self.scroll.setWidget(self.tab_widgets[0])
 
     def change_tab(self, ind):
+        self.tab_ind = ind
         self.layout.removeWidget(self.scroll)
         self.scroll = Qw.QScrollArea(self.main_window)
+        self.scroll.setWidgetResizable(True)
         self.layout.addWidget(self.scroll)
         self.tab_widgets[ind].update_info()
         self.scroll.setWidget(self.tab_widgets[ind])
+
+    def update_info(self):
+        self.tab_widgets[self.tab_ind].update_info()
+
+    def passive_update(self):
+        pass
+
+    def read_settings(self):
+        self.settings = Qc.QSettings()
+        self.update_frequency = self.settings.value('update frequency', 10)
+        self.passive_period = self.settings.value('passive period', 5)
+        if self.timing:
+            self.kill_timers()
+            self.init_timers()
+            self.start_timers()
 
     def show(self):
         self.main_window.show()
@@ -121,6 +173,8 @@ def except_hook(cls, exception, traceback):
 if __name__ == '__main__':
     sys.excepthook = except_hook
     app = Qw.QApplication(sys.argv)
+    app.setApplicationName(APP_NAME)
+    app.setOrganizationName(ORG_NAME)
     main = Main()
     main.show()
     sys.exit(app.exec())
